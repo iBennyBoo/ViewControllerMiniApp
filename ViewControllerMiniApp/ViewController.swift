b545856529a34b599b0f563ba38d8c60b23c490b
Ben Berman <bpb062004@gmail.com>
2021-09-21 16:12:30 -0500

    Initial Commit

---
+++ ViewControllerMiniApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ViewControllerMiniApp
+//
+//  Created by Benjamin Berman on 9/21/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
